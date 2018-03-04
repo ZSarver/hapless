f4692dd5d7cfa1c3063b4821c07d3f17542c3e2e
Zachary Sarver <Zachary.Sarver@gmail.com>
2018-03-04 14:44:24 -0500

    Use cards

--- src/Main.purs
+++ src/Main.purs
@@ -4,10 +4,12 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import RotFFI (DisplayOptions(..), init, ROT)
+import Cards (attack)
 
 main :: forall e. Eff ( console :: CONSOLE, rot :: ROT | e) Unit
 main = do
   log "Hello sailor!"
   let opts = DisplayOptions { width: 8.0, height: 8.0 }
   display <- init opts
+  let card = attack
   pure unit
