4601741899c2366a4840bb054ed605de084fc4cb
Zachary Sarver <Zachary.Sarver@gmail.com>
2018-03-05 22:20:14 -0500

    Move effect implemented

--- src/GameState.purs
+++ src/GameState.purs
@@ -3,8 +3,11 @@ module GameState where
 import Batteries
 
 import Box
-import PlayerData (Player, dummyPlayer)
+import PlayerData (Player(..), dummyPlayer)
 import EnemyData (Enemy, dummyEnemy)
+import Optic.Lens
+import Optic.Types
+import XY
 
 newtype GameState = GameState
   { player :: Player
@@ -22,6 +25,20 @@ serialize g = genericEncodeJSON defaultOptions g
 deserialize :: String -> _ GameState
 deserialize s = genericDecodeJSON defaultOptions s
 
+setLocation :: GameState -> XY -> GameState
+setLocation (GameState g) l = GameState g'
+  where
+    (Player p) = g.player
+    p' = p {location = l}
+    g' = g {player = Player p'}
+
+getLocation :: GameState -> XY
+getLocation (GameState g) = p.location
+  where
+    (Player p) = g.player
+
+locationLens :: Lens' GameState XY
+locationLens = lens (getLocation) (setLocation)
 
 dummyGameState :: GameState
 dummyGameState = GameState { player: dummyPlayer, enemies: [dummyEnemy], boundaries: Box {width: 6, height: 6} }
